537f02ecd8e4a40f64e8b177958bc1f7bc4a5f7c
izaacMBD <izaacMB202825@proton.me>
2024-10-29 22:16:25 -0300

    UP

--- aula04-teorica.py
+++ aula04-teorica.py
@@ -1,30 +1,68 @@
-#Classe com atributos (dados) e métodos (funções)
-class Cachorro:
+def exemplo01():
+        #Classe com atributos (dados) e métodos (funções)
+    class Cachorro:
+        #self explicita determinada istância da classe que esta sendo estipulada
+        #Método01 Construtor, precisa de __init__ para ser reconhecido como Método construtor
+        def __init__(self, nome, idade): 
+            self.nome = nome
+            self.idade = idade
+
+        #Método02
+        def latir(self):
+            print(f"{self.nome} está latindo")
+            
+        #Método02
+        def idade_de_cachorro(self):
+            print(f"{self.nome} tem {self.idade} anos de vida, e está latindo")
+            
+
+    nome_do_cao = input("digite o name: ")
+    idade_do_cao = int(input("qual é a idade: "))
+
+    #Criando um objeto da Classe Cachorro
+    meu_cachorro = Cachorro(nome_do_cao, idade_do_cao) #Quando uma classe e posta em uma váriavel chamamos-a de Instância
+    print(meu_cachorro.nome) #Acessa o atributo 'nome'
+    meu_cachorro.latir() #Chamando o método latir
+    meu_cachorro.idade_de_cachorro()
+
+
+def exemplo1_test():
+    class Gato:
     #self explicita determinada istância da classe que esta sendo estipulada
     #Método01 Construtor, precisa de __init__ para ser reconhecido como Método construtor
-    def __init__(self, nome, idade): 
-        self.nome = nome
-        self.idade = idade
+        def __init__(self, nome, idade): 
+            self.nome = nome
+            self.idade = idade
 
-    #Método02
-    def latir(self):
-        print(f"{self.nome} está latindo")
+        #Método02
+        def miar(self):
+            print(f"{self.nome} está miando")
+            
+        #Método02
+        def idade_de_gato(self):
+            print(f"{self.nome} tem {self.idade} anos de vida, e está miando")
         
-    #Método02
-    def idade_de_cachorro(self):
-        print(f"{self.nome} tem {self.idade} anos de vida, e está latindo")
-        
 
-nome_do_cao = input("digite o name: ")
-idade_do_cao = int(input("qual é a idade: "))
-digite = int(input("Digite 0 ou 1: "))
+    nome_do_cao = input("digite o name: ")
+    idade_do_cao = int(input("qual é a idade: "))
+
+    #Criando um objeto da Classe gato
+    meu_gato = Gato(nome_do_cao, idade_do_cao) #Quando uma classe e posta em uma váriavel chamamos-a de Instância
+    print(meu_gato.nome) #Acessa o atributo 'nome'
+    meu_gato.miar() #Chamando o método miar
+    meu_gato.idade_de_gato()
 
 
-#Criando um objeto da Classe Cachorro
-meu_cachorro = Cachorro(nome_do_cao, idade_do_cao) #Quando uma classe e posta em uma váriavel chamamos-a de Instância
-numeros = [meu_cachorro.idade_de_cachorro(), meu_cachorro.latir()]
+def exemplo02():
+    class Classielison:
+        variavel_de_classe = 0
+        def __init__(self):
+            Classielison.variavel_de_classe += 1
+    a = Classielison()
+    b = Classielison()
+    print(Classielison.variavel_de_classe)
 
-if digite != 0 and digite != 1:
-    print("Vai se fuder")
-else:
-    print(numeros(digite))
+
+# exemplo01()
+# exemplo1_test()
+exemplo02()
